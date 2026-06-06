e2061ca4cb41247fe2b5cbff5eb0cc352277e78f
AmrYasserSaber <amr.yassr.2@gmail.com>
2026-06-06 15:17:08 +0300

    add CRUD actions for posts in PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,53 @@
 class PostsController < ApplicationController
-  # your code goes here
+  # GET /posts or /posts.json
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/1 or /posts/1.json
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # GET /posts/1/edit
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  # POST /posts or /posts.json
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "Post was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /posts/1 or /posts/1.json
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(title: params[:post][:title], content: params[:post][:content])
+      redirect_to @post, notice: "Post was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /posts/1 or /posts/1.json
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to @post , notice: "Post was successfully destroyed."
+  end
+
+  private
+    def post_params
+      params.expect(post: [ :title, :content ])
+    end
 end

b0fdf8f646c76ba1281298dd1c084600715621a8
AmrYasserSaber <amr.yassr.2@gmail.com>
2026-06-06 15:17:19 +0300

    add users controller with CRUD actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+class UsersController < ApplicationController
+  # GET /users or /users.json
+  def index
+    @users = User.all
+  end
+
+  # GET /users/1 or /users/1.json
+  def show
+    @user = User.find(params[:id])
+  end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # GET /users/1/edit
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  # POST /users or /users.json
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user, notice: "User was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /users/1 or /users/1.json
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: "User was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/1 or /users/1.json
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to @user , notice: "User was successfully destroyed."
+  end
+
+  private
+  def user_params
+    params.expect(user: [ :name, :email ])
+  end
+end

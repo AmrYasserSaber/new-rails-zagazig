725373db4a92565a78fdd93715a2672d3956d4bf
AmrYasserSaber <amr.yassr.2@gmail.com>
2026-06-08 10:45:41 +0300

    Add user and post models with migrations, forms, and controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,39 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Like.destroy_all
+Post.destroy_all
+User.destroy_all
+
+puts "Creating Users..."
+5.times do |i|
+  User.create!(
+    name: "User #{i + 1}"
+  )
+end
+
+all_users = User.all.to_a
+
+puts "Creating Posts..."
+50.times do |i|
+  Post.create!(
+    title: "Post #{i + 1}",
+    body: "This is the content of post #{i + 1}.",
+    user: all_users.sample
+  )
+end
+
+all_posts = Post.all.to_a
+
+puts "Creating likes..."
+500.times do
+  Like.find_or_create_by!(
+    user: all_users.sample,
+    post: all_posts.sample
+  )
+end
+
+puts "Database seeded successfully!"
+puts "Created #{User.count} Users, #{Post.count} Posts, and #{Like.count} Likes."
